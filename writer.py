b32fcf8e651f275559f41fcd981a8e32e6865af5
natpev <natpev@ymail.com>
2020-11-11 20:32:07 -0500

    made writer more 'auto'

--- writer.py
+++ writer.py
@@ -1,11 +1,12 @@
 #!/bin/python3
 import signal
+import os
 from os import system
 
-signal.signal(signal.SIGINT, signal.SIG_IGN)
-signal.signal(signal.SIGTSTP, signal.SIG_IGN)
+#signal.signal(signal.SIGINT, signal.SIG_IGN)
+#signal.signal(signal.SIGTSTP, signal.SIG_IGN)
 
-quiz = 'quiz_files/Pentest+_questions.txt'
+quiz = 'x'
 
 resp_bank = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 resp_num = 0
@@ -14,68 +15,111 @@ char = ['','\n']
 first = 0
 
 def clear():
-    _ = system('clear')
+    pass
+#    _ = system('clear')
+
+
+new = input('New quiz? Y|N  ')
+
+while True:
+    if new.upper() in ans_bank:
+        break
+    else:
+        print('INVALID')
+        new = input('New quiz? Y|N  ')
+if new.upper() == 'Y':
+    quiz = ('quiz_files/' + input('filename? (without extension)  :') + '.txt')
+    title = (input('quiz title  :'))
+    with open(quiz, mode='a') as myfile:
+        myfile.write('QUIZ:' + title + '\nTOTAL:\n\n\n\n')
+    q_num = 1
+
+
+
+
+
+
+
+
+
+else:            ##continue old quiz
+    print('\n')
+    filelist = (os.listdir(os.getcwd()+ '/quiz_files'))
+    for i in filelist:
+        print(i)
+    quiz = input('\nchoose quiz to edit  :')
+    while True:
+        if quiz in filelist:
+            break
+        else:
+            quiz = input('choose quiz to edit  :')
+    quiz = ('quiz_files/' + quiz)
+    flength = sum(1 for line in open(quiz))
+    print(flength)
+    for line in reversed(open(quiz).readlines()):
+        if 'QUESTION' in line:
+            q_num = (int(line[8:]) + 1)
+            break
+
+
+
+
+
 
 try:
-        clear()
-        while True:
-            try:
-                q_num = int(input('starting point: '))
-                if int(q_num) < 1:
-                    raise ValueError('value too small')
-                else:
-                    break
-            except:
-                print('INVALID')
-        clear()
-        while True:
-            addq = input('\nadd question Y|N : ')
-            while True:
-                if addq.upper() not in ans_bank:
-                    addq = input('\nadd question Y|N : ')
-                else:
-                    break
-            clear()
-            if addq.upper() == 'Y':
-                question = input('\nenter question: ' + str(q_num) + '\n')
-                question = question.replace('\\n', '\n')
-                with open(quiz, mode='a') as myfile:
-                    myfile.write(char[first] + '\n\nQUESTION ' + str(q_num) + '\n\n' + question + '\n\n')
-                first = 1
-            else:
-                clear()
-                print('\n\ndone at ' + str(q_num - 1))
-                break
+    clear()
 
-            while True:
-                while True:
-                    try:
-                        adda = input('number of responses : ')
-                        if int(adda) >26:
-                            raise ValueError("value too great")
-                        elif int(adda) <1:
-                            raise ValueError("value too small")
-                        else:
-                            print(adda)
-                            break
-                    except:
-                        print('INVALID')
-                while  int(adda) > 0:
-                    resp = input('\nenter option  ' + resp_bank[resp_num] + '. ')
-                    with open(quiz,mode='a') as myfile:
-                        myfile.write(resp_bank[resp_num] + '. ' + resp + '\n')
-                    resp_num += 1
-                    adda = (int(adda) - 1)
-                else:
-                    correct = input('\ncorrect answer: ')
-                    with open(quiz,mode='a') as myfile:
-                        myfile.write('\nCorrect Answer: ' + correct.upper())
-                    resp_num = 0
-                    q_num = (int(q_num) + 1)
-                    break
-            expl = input('Explination: ')
-            with open(quiz,mode='a') as myfile:
-                myfile.write('\n\nExplination: ' + expl)
+    while True:
+        addq = input('\nadd question Y|N : ')
+        while True:
+            if addq.upper() not in ans_bank:
+                addq = input('\nadd question Y|N : ')
+            else:
+                break
+        clear()
+        if addq.upper() == 'Y':
+            question = input('\nenter question: ' + str(q_num) + '\n')
+            question = question.replace('\\n', '\n')
+            with open(quiz, mode='a') as myfile:
+                myfile.write(char[first] + '\n\nQUESTION ' + str(q_num) + '\n\n' + question + '\n\n')
+            first = 1
+        else:
+            with open(quiz, mode='a') as myfile:
+                myfile.write('\n')
             clear()
-except:
-    pass
+            print('\n\ndone at ' + str(q_num - 1))
+            break
+
+        while True:
+            while True:
+                try:
+                    adda = input('number of responses : ')
+                    if int(adda) >26:
+                        raise ValueError("value too great")
+                    elif int(adda) <1:
+                        raise ValueError("value too small")
+                    else:
+                        print(adda)
+                        break
+                except:
+                    print('INVALID')
+            while  int(adda) > 0:
+                resp = input('\nenter option  ' + resp_bank[resp_num] + '. ')
+                with open(quiz,mode='a') as myfile:
+                    myfile.write(resp_bank[resp_num] + '. ' + resp + '\n')
+                resp_num += 1
+                adda = (int(adda) - 1)
+            else:
+                correct = input('\ncorrect answer: ')
+                with open(quiz,mode='a') as myfile:
+                    myfile.write('\nCorrect Answer: ' + correct.upper())
+                resp_num = 0
+                q_num = (int(q_num) + 1)
+                break
+        expl = input('Explination: ')
+        with open(quiz,mode='a') as myfile:
+            myfile.write('\n\nExplination: ' + expl)
+        clear()
+except KeyboardInterrupt:
+    print('hi')
+
